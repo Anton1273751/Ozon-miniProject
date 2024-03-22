1e22f212c4f9ac8d2812a1c7443095b234190519
Anton1273751 <kronoss161@gmail.com>
2024-03-22 09:32:39 +0300

    add module

--- src/app/index.js
+++ src/app/index.js
@@ -1,46 +1,53 @@
-const consoles = [
-  {
-    name: "Игровая приставка Sony PlayStation 4 Pro",
-    price: 33990,
-    img: "./src/image/span.card-img-top.jpeg",
-  },
-  {
-    name: "Игровая приставка Sony PlayStation 3 Super Slim",
-    price: 16499,
-    img: "./src/image/Ps3.jpeg",
-  },
-  {
-    name: "Игровая приставка Xbox One X",
-    price: 39990,
-    img: "./src/image/XboxOneX.jpeg",
-  },
+import { consoles } from "./test.js";
+import { goods } from "./db.js";
+// import { goods } from "./src/app/db.mjs";
 
-  {
-    name: "Игровая приставка Nintendo Switch",
-    price: 24751,
-    img: "./src/image/nintendo.jpeg",
-  },
-  {
-    name: "Игровая приставка Sega Retro Genesis HD",
-    price: 3624,
-    img: "./src/image/sega.jpeg",
-  },
-  {
-    name: "Игровая приставка Dendy Junior",
-    price: 1551,
-    img: "./src/image/dendy.jpeg",
-  },
-  {
-    name: "Игровая приставка Sony PlayStation Classic",
-    price: 10445,
-    img: "./src/image/psClassic.jpeg",
-  },
-  {
-    name: "Игровая приставка Xbox One S ",
-    price: 23411,
-    img: "./src/image/XboxOneS.jpeg",
-  },
-];
+console.log(consoles);
+console.log(goods);
+
+// const consoles = [
+//   {
+//     name: "Игровая приставка Sony PlayStation 4 Pro",
+//     price: 33990,
+//     img: "./src/image/span.card-img-top.jpeg",
+//   },
+//   {
+//     name: "Игровая приставка Sony PlayStation 3 Super Slim",
+//     price: 16499,
+//     img: "./src/image/Ps3.jpeg",
+//   },
+//   {
+//     name: "Игровая приставка Xbox One X",
+//     price: 39990,
+//     img: "./src/image/XboxOneX.jpeg",
+//   },
+
+//   {
+//     name: "Игровая приставка Nintendo Switch",
+//     price: 24751,
+//     img: "./src/image/nintendo.jpeg",
+//   },
+//   {
+//     name: "Игровая приставка Sega Retro Genesis HD",
+//     price: 3624,
+//     img: "./src/image/sega.jpeg",
+//   },
+//   {
+//     name: "Игровая приставка Dendy Junior",
+//     price: 1551,
+//     img: "./src/image/dendy.jpeg",
+//   },
+//   {
+//     name: "Игровая приставка Sony PlayStation Classic",
+//     price: 10445,
+//     img: "./src/image/psClassic.jpeg",
+//   },
+//   {
+//     name: "Игровая приставка Xbox One S ",
+//     price: 23411,
+//     img: "./src/image/XboxOneS.jpeg",
+//   },
+// ];
 
 const wrapperNode = document.querySelector(".wrapper");
 
